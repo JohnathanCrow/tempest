89cc304ab84797833c23628d5d2f9d1e5138da28
JohnathanCrow <JohnathanCrow@live.com>
2026-05-18 17:13:22 +0100

    cache setting
    fix
      - properly fetch sw.js

--- app.js
+++ app.js
@@ -1262,7 +1262,7 @@ window.addEventListener("resize", () => {
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-		navigator.serviceWorker.register("./sw.js").catch(() => {});
+		navigator.serviceWorker.register("./sw.js", { updateViaCache: "none" }).catch(() => {});
 	});
 }
 
